5b81d47de1367b2b8d0709a4a5946a7e1c512372
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-22 14:04:12 +0200

    Add configuration to deploy github pages

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:liberostelios/cityjson-viewer-vue.git master:gh-pages
+
+cd -
