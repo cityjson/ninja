1fc15f957a52b56ec68e2335495b0ead218ebbf2
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-23 15:34:27 +0200

    Fix deploy script to reflect the new name

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:liberostelios/cityjson-viewer-vue.git master:gh-pages
+git push -f git@github.com:liberostelios/ninja.git master:gh-pages
 
 cd -
