e9928e8123693489018db1fc39a7e086442a6fdb
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-23 10:59:00 +0200

    Change default colour of TINRelief

--- src/App.vue
+++ src/App.vue
@@ -160,7 +160,7 @@ export default {
         "Railway": 0x000000,
         "Road": 0x999999,
         "SolitaryVegetationObject": 0x39ac39,
-        "TINRelief": 0xe3eaf8,
+        "TINRelief": 0xffdb99,
         "TransportSquare": 0x999999,
         "Tunnel": 0x999999,
         "TunnelPart": 0x999999,
